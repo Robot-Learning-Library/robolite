4f8b232b5ac7036d439fb29861ee790d8fc7fb08
zihan <zhding@mail.ustc.edu.cn>
2020-11-11 15:54:26 +0800

    add a simple ik without using pykdl

---
+++ robosuite/class_wrappers/panda_ik_simple_wrapper.py
@@ -0,0 +1,168 @@
+"""
+Panda inverse kinematics using PyKDL, core by Zihan Ding
+Currently only position control is supported. (that means we have to pass a fixed pose for the end effector)
+When 3-d space is accomodated, the dof is 3. When 2-d space is accomodated, a separate fixed z coordinate should be provided.
+If limit_range is provided as [[xl, xr], [yl, yr]], the ik wrapper will try hard to limit the robot arm within the plane range.
+"""
+
+import numpy as np
+from . import change_dof
+from mujoco_py import functions
+from gym.envs.robotics.rotations import quat2euler, euler2quat, mat2euler, mat2quat, quat_mul, quat_conjugate
+
+def mul_quat(quat1, quat2):
+    quat1 = np.array(quat1)
+    quat2 = np.array(quat2)
+    res = np.array([1., 0., 0., 0.])
+    functions.mju_mulQuat(res, quat1, quat2)
+    return res
+
+def from_matrix(matrix):
+
+    is_single = False
+    matrix = np.asarray(matrix, dtype=float)
+
+    if matrix.ndim not in [2, 3] or matrix.shape[-2:] != (3, 3):
+        raise ValueError("Expected `matrix` to have shape (3, 3) or "
+                        "(N, 3, 3), got {}".format(matrix.shape))
+
+    # If a single matrix is given, convert it to 3D 1 x 3 x 3 matrix but
+    # set self._single to True so that we can return appropriate objects in
+    # the `to_...` methods
+    if matrix.shape == (3, 3):
+        matrix = matrix.reshape((1, 3, 3))
+        is_single = True
+
+    num_rotations = matrix.shape[0]
+
+    decision_matrix = np.empty((num_rotations, 4))
+    decision_matrix[:, :3] = matrix.diagonal(axis1=1, axis2=2)
+    decision_matrix[:, -1] = decision_matrix[:, :3].sum(axis=1)
+    choices = decision_matrix.argmax(axis=1)
+
+    quat = np.empty((num_rotations, 4))
+
+    ind = np.nonzero(choices != 3)[0]
+    i = choices[ind]
+    j = (i + 1) % 3
+    k = (j + 1) % 3
+
+    quat[ind, i] = 1 - decision_matrix[ind, -1] + 2 * matrix[ind, i, i]
+    quat[ind, j] = matrix[ind, j, i] + matrix[ind, i, j]
+    quat[ind, k] = matrix[ind, k, i] + matrix[ind, i, k]
+    quat[ind, 3] = matrix[ind, k, j] - matrix[ind, j, k]
+
+    ind = np.nonzero(choices == 3)[0]
+    quat[ind, 0] = matrix[ind, 2, 1] - matrix[ind, 1, 2]
+    quat[ind, 1] = matrix[ind, 0, 2] - matrix[ind, 2, 0]
+    quat[ind, 2] = matrix[ind, 1, 0] - matrix[ind, 0, 1]
+    quat[ind, 3] = 1 + decision_matrix[ind, -1]
+
+    quat /= np.linalg.norm(quat, axis=1)[:, None]
+
+    if is_single:
+        return quat[0]
+        # return cls(quat[0], normalize=False, copy=False)
+    else:
+        # return cls(quat, normalize=False, copy=False)
+        return quat
+
+def trans2posquat(tform):
+    pos = (tform[0:3, 3]).transpose()
+    # a = R.identity(3)
+    # R.from_rotvec()
+    # r = R.from_matrix(tform[0:3, 0:3])
+    # quat = r.as_quat()
+    # quat = np.hstack((quat[3], quat[0:3]))
+    # print('--------------')
+    # print(quat)
+    quat = from_matrix(tform[0:3, 0:3])
+    quat = np.hstack((quat[3], quat[0:3]))
+    # print(quat)
+    return pos, quat
+
+def quat2vel(quat, dt):
+    quat = np.array(quat)
+    res = np.array([0., 0., 0.])
+    functions.mju_quat2Vel(res, quat, dt)
+    return res
+
+def conj_quat(quat):
+    # w x y z
+    quat = np.array(quat)
+    res = np.array([1., 0., 0., 0.])
+    functions.mju_negQuat(res, quat)
+    return res
+
+def panda_ik_simple_wrapper(Env, rotation = False, fix_z=None, max_action=0.5, pose_mat=None, limit_range=None):
+    ik_dof = 2 if fix_z is not None else 3
+    if rotation:   # if allowing rotation control for EE, add 3 dims of euler for EE orientation
+        ik_dof += 3
+    
+    wrapping_dof = (Env.dof - 7) + ik_dof
+    
+    class PandaIK(change_dof(Env, wrapping_dof)):
+        parameters_spec = {
+            **Env.parameters_spec,
+            'pandaik_z_proportional_gain': [1.5, 2.5],
+        } if fix_z is not None else Env.parameters_spec
+
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self.z_prop_gain = 2.0
+            self._set_reference_right_hand_orn()
+
+        def _set_reference_right_hand_orn(self):
+            if pose_mat is not None:
+                self.reference_right_hand_orn = pose_mat
+            else:
+                self.reference_right_hand_orn = self._right_hand_orn.copy()
+
+        def reset(self, **kwargs):
+            ret = super().reset(**kwargs)
+            self._set_reference_right_hand_orn()
+            return ret
+
+        def reset_props(self, pandaik_z_proportional_gain=2.0, **kwargs):
+            self.z_prop_gain = pandaik_z_proportional_gain
+            super().reset_props(**kwargs)
+
+        def jac_geom(self, geom_name):
+            jacp = self.sim.data.get_geom_jacp(geom_name)  # 3-dimensional position
+            jacr = self.sim.data.get_geom_jacr(geom_name)  # 3-dimensional orientation
+            jacp = jacp.reshape(3, -1)  
+            jacr = jacr.reshape(3, -1)
+            # print(np.vstack((jacp, jacr)))
+            return np.vstack((jacp[:, :7], jacr[:, :7]))
+
+        def get_geom_posquat(self, name):
+            rot = self.sim.data.get_geom_xmat(name)
+            tform = np.eye(4)
+            tform[0:3, 0:3] = rot
+            tform[0:3, 3] = self.sim.data.get_geom_xpos(name).transpose()
+            pos, quat = trans2posquat(tform)
+            return np.hstack((pos, quat))
+
+        def step(self, action_all):
+            assert(action_all.shape == (self.dof, ))
+            action = action_all[:ik_dof]  # the first 3 dims are position, and the last 3 dims are orientation (euler)
+            action_other = action_all[ik_dof:]  # gripper control, etc
+
+            action = np.clip(action, np.ones(ik_dof) * -1, np.ones(ik_dof) * 1) * max_action   # action range: [-max_action, max_action]
+
+            # ee_curr = self.get_geom_posquat("hand_visual")  # does not give a fully correct control: correct for position and x- and y-rotation, wrong for z-rotation
+            ee_curr = np.concatenate([self._right_hand_pos, mat2quat(self._right_hand_orn)])  # gives correct control for both opsition and orientation
+
+            ee_tget = action + np.concatenate([ee_curr[:-4], quat2euler(ee_curr[-4:])])    # change last 3 dims for orientation from quaternion to euler
+            ee_tget = np.concatenate([ee_tget[:-3], euler2quat(ee_tget[-3:])])  # change last 3 dims for orientation from euler to quaternion
+
+            ee_jac = self.jac_geom("hand_visual")  # get the jacobian w.r.t. a geom with its name
+            vel = np.hstack(((ee_tget[:3] - ee_curr[:3]) / 5,    # divided by 5 to generate small action
+                                quat2vel(mul_quat(ee_tget[-4:], conj_quat(ee_curr[-4:])), 1)))   # difference of quaternions is calculated with  M^(-1)^T * N: inverse transpose is conjugate 
+            qvel = np.matmul(np.linalg.pinv(ee_jac), vel.transpose())
+            
+            final_action = np.concatenate([np.asarray(qvel).squeeze(), action_other])
+
+            return super().step(final_action)
+
+    return PandaIK
