672572efa2abff3323e6a0e586a58ad38b077fbd
zihan <zhding@mail.ustc.edu.cn>
2020-11-17 15:39:45 +0800

    t

--- robosuite/models/assets/objects/mujoco_load_xml_test.py
+++ robosuite/models/assets/objects/mujoco_load_xml_test.py
@@ -1,7 +1,8 @@
 import mujoco_py
 from mujoco_py import load_model_from_path, MjSim, MjViewer
 import os
-model = load_model_from_path('./cabinet.xml')
+# model = load_model_from_path('./cabinet.xml')
+model = load_model_from_path('./soft_cuboid.xml')
 
 sim = mujoco_py.MjSim(model)
 
